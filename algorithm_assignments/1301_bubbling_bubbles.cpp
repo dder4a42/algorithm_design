625bbf252d11401eb88af3e34aa0bac6d1ca49db
deng4a42 <deng4a42@admin>
2024-10-19 21:40:13 +0800

    [improve] 1301. a faster way which count inversions when merge, make O(2n) to O(n) for combining subproblems.

--- algorithm_assignments/1301_bubbling_bubbles.cpp
+++ algorithm_assignments/1301_bubbling_bubbles.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int n;
 // int* origin;
 vector<int> arr;
+vector<int> arr_fast;
 vector<int> table;
+vector<int> table_fast;
 
 void solve(int l, int r) {
     // solve arr[l,r]
@@ -59,6 +61,48 @@ void solve(int l, int r) {
     // for(int i=0;i<n;++i) cout << table[origin[i]] << " \n"[i==n-1];
 }
 
+void solve_fast(int l, int r) {
+    // solve arr[l,r]
+    if(l >= r) return ;
+    int mid = (r-l) / 2 + l;
+    solve_fast(l, mid);
+    solve_fast(mid+1, r);
+
+    int i=l, j=mid+1, k=0;
+    int* tmp = new int [r-l+1];
+    while(i<=mid && j<=r) {
+        if(arr_fast[i] <= arr_fast[j]) {
+            tmp[k] = arr_fast[i];
+            table_fast[arr_fast[i]] += j-mid-1;
+            i++;
+        }else {
+            tmp[k] = arr_fast[j];
+            table_fast[arr_fast[j]] += mid+1-i;
+            j++;
+        }
+        k++;
+    }
+    while(i<=mid) {
+        tmp[k] = arr_fast[i];
+        table_fast[arr_fast[i]] += j-mid-1;
+        i++, k++;
+    }
+    while (j<=r) {
+        tmp[k] = arr_fast[j];
+        table_fast[arr_fast[j]] += mid+1-i;
+        j++, k++;
+    }
+    for(int i=l;i<=r;i++) {
+        arr_fast[i] = tmp[i-l];
+    }
+    delete [] tmp;
+    
+    // cout << "arr[" << l << ":" << r << "] is ";
+    // for(int i=l;i<=r;i++) cout << arr[i] << " \n"[i == r];
+    // cout << "number count is ";
+    // for(int i=0;i<n;++i) cout << table[origin[i]] << " \n"[i==n-1];
+}
+
 void bubble_sort(int a[], int n) {
     vector<int> ret(n+1, 0);
     for (int i = 1; i < n; i++) {
@@ -80,12 +124,15 @@ int main() {
     
     cin >> n;
     // origin = new int [n];
-    table.resize(n+1);
+    // table.resize(n+1);
+    table_fast.resize(n+1);
     for(int i=0;i<n;++i) {
         int x;
         cin >> x;
-        arr.push_back(x);
-        table[i+1]=0;
+        // arr.push_back(x);
+        arr_fast.push_back(x);
+        // table[i+1]=0;
+        table_fast[i+1]=0;
         // origin[i] = x;
     }
 
@@ -95,9 +142,13 @@ int main() {
     // for(int i=0;i<n;++i) cout << table[origin[i]] << " \n"[i==n-1];
 
     solve(0, arr.size()-1);
+    solve_fast(0, arr_fast.size()-1);
     // bubble_sort(origin, n);
+    // for(int i=1;i<=n;++i) {
+    //     cout << table[i] << " \n"[i==n];
+    // }
     for(int i=1;i<=n;++i) {
-        cout << table[i] << " \n"[i==n];
+        cout << table_fast[i] << " \n"[i==n];
     }
     // delete [] origin;
     return 0;
