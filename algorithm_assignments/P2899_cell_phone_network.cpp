c2d47d8dc0b097d35264ad72752f3be237c98a92
deng4a42 <deng4a42@admin>
2024-11-22 12:43:17 +0800

    [AC] P2899, cover vertex set merge operations on cover vertex into one place, use is-cover-vertex flag to triggle covering other vertices.

--- algorithm_assignments/P2899_cell_phone_network.cpp
+++ algorithm_assignments/P2899_cell_phone_network.cpp
@@ -5,25 +5,24 @@ using namespace std;
 int num_cover_vertex = 0;
 vector<int> solution;
 
-void dfs(int root, vector<vector<int>>& adj_list, vector<bool>& is_visited, vector<bool>& is_covered, vector<bool>& is_cover_vertex) {
+void dfs(int root, vector<vector<int>>& adj_list, vector<bool>& is_visited, vector<bool>& is_covered, vector<bool>& is_cover_vertex, int parent) {
     is_visited[root] = true;
     if(adj_list[root].size() == 1 && root != 0) {
         // cout << "find leaf " << root << endl;
-        int parent = adj_list[root][0];
-        if(is_cover_vertex[parent] == false) {
-            // cout << "add cover vertex " << parent << endl;
-            is_covered[parent] = true;
-            is_cover_vertex[parent] = true;
-            num_cover_vertex++;
-            solution.push_back(parent);
-            for(auto be_covered: adj_list[parent]) {
-                is_covered[be_covered] = true;
-            }
-        }
+        // cout << "add cover vertex " << parent << endl;
+        is_cover_vertex[parent] = true;
     }
     for(auto child: adj_list[root]) {
         if(is_visited[child] == false) {
-            dfs(child, adj_list, is_visited, is_covered, is_cover_vertex);
+            dfs(child, adj_list, is_visited, is_covered, is_cover_vertex, root);
+        }
+    }
+    if(is_cover_vertex[root]) {
+        is_covered[root] = true;
+        num_cover_vertex++;
+        solution.push_back(root);
+        for(auto be_covered: adj_list[root]) {
+            is_covered[be_covered] = true;
         }
     }
     if(is_covered[root] == false) {
@@ -32,21 +31,7 @@ void dfs(int root, vector<vector<int>>& adj_list, vector<bool>& is_visited, vect
             num_cover_vertex++;
             solution.push_back(root);
         }else {
-            int parent;
-            for(auto x: adj_list[root]) {
-                if(is_covered[x] == false) {
-                    parent = x;
-                    break;
-                }
-            }
-            // cout << "add cover vertex " << parent << endl;
-            is_covered[parent] = true;
             is_cover_vertex[parent] = true;
-            num_cover_vertex++;
-            solution.push_back(parent);
-            for(auto be_covered: adj_list[parent]) {
-                is_covered[be_covered] = true;
-            }
         }
     }
     is_covered[root] = true;
@@ -68,7 +53,7 @@ int main() {
         adj_list[u-1].push_back(v-1);
         adj_list[v-1].push_back(u-1);
     }
-    dfs(0, adj_list, is_visited, is_covered, is_cover_vertex);
+    dfs(0, adj_list, is_visited, is_covered, is_cover_vertex, 0);
 
     // for(auto x: solution) {
     //     cout << x << " ";
