86d96a57ba91832a31e5589893df475bae4f5790
deng4a42 <deng4a42@admin>
2024-12-17 23:31:05 +0800

    [TLE] 1433, stock have a well defined state, but the running time fails. need a 1D dp[] or other improvement tech

--- algorithm_assignments/1433_stock.cpp
+++ algorithm_assignments/1433_stock.cpp
@@ -3,51 +3,44 @@
 #include<algorithm>
 using namespace std;
 
+const int INF = 1E9;
+
 int main() {
     // ios::sync_with_stdio(false);
     // cin.tie(nullptr);
 
     int n;
     cin >> n;
-    vector<int> prices(n+1, 0);
-    for(int i=1;i<=n;i++) {
+    vector<int> prices(n, 0);
+    for(int i=0;i<n;i++) {
         cin >> prices[i];
     }
+    int k = n/2;
+    vector<vector<int>> dp(n, vector<int> (k+1, 0));
+
+    dp[0][0] = 0;
+    dp[0][1] = -prices[0];
+    for(int j=2;j<=k;j++) {
+        dp[0][j] = -INF;
+    }
+
+    for(int i=1;i<n;i++) {
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i]);
+        for(int j=1;j<k;j++) {
+            // nothing to do
+            //dp[i][j] = dp[i-1][j];
+            // buy stock
+            //dp[i][j] = dp[i-1][j-1] - prices[i];
+            // sell stock
+            //dp[i][j] = dp[i-1][j+1] + prices[i];
+            dp[i][j] = std::max({dp[i-1][j], dp[i-1][j-1] - prices[i], dp[i-1][j+1] + prices[i]});
+        }
+        dp[i][k] = std::max(dp[i-1][k], dp[i-1][k-1] - prices[i]);
+    }
+
+    cout << dp[n-1][0] << endl;
 
     
-
-    vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (3, vector<int> (n/2+1, 0)));
-    int k = n/2+1;
-    // dp[0][0][0] = -1E7;
-    // dp[0][1][0] = -1E7;
-    // dp[0][2][0] = 0;
-    // for(int i=1;i<=n/2;i++) {
-    //     dp[0][0][i] = -1E7;
-    //     dp[0][1][i] = -1E7;
-    // }
-
-    // 0 buy, 1 sell, 2 none
-    dp[1][0][0] = -prices[1];
-    dp[1][2][0] = 0;
-    for(int i=2;i<=n;i++) {
-        dp[i][2][0] = std::max({dp[i-1][1][1], dp[i-1][2][0]});
-        // dp[i][1][0] = -1E7;
-        dp[i][0][0] = dp[i][2][0] - prices[i];
-        for(int j=1;j<n/2;j++) {
-            dp[i][2][j] = max({dp[i-1][2][j], dp[i-1][0][j-1], dp[i-1][1][j+1]});
-            dp[i][1][j] = dp[i][2][j] + prices[i];
-            dp[i][0][j] = dp[i][2][j] - prices[i];
-        }
-        dp[i][2][n/2] = max({dp[i-1][2][n/2], dp[i-1][0][n/2-1]});
-        dp[i][1][n/2] = dp[i][2][n/2] + prices[i];
-        dp[i][0][n/2] = dp[i][2][n/2] - prices[i];
-    }
-
-    int max_one = 0;
-    for(int i=0;i<n/2+1;i++) {
-        max_one = max({dp[n-1][0][i], dp[n-1][2][i], max_one});
-    }
-    cout << max_one << endl;
     
     return 0;
 }
