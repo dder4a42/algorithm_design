013b371beee1db7dea9ee27ff8a1535791efc04d
deng4a42 <deng4a42@admin>
2024-12-17 12:56:32 +0800

    [update] stock4 2D array dp[][] works well.

--- algorithm_assignments/lc188_stock4.cpp
+++ algorithm_assignments/lc188_stock4.cpp
@@ -8,6 +8,35 @@ int main() {
 
     int n, k;
     cin >> n >> k;
+    vector<vector<int>> dp(n, vector<int> (2*k, 0));
+    vector<int> prices(n, 0);
+    for(int i=0;i<n;i++) {
+        cin >> prices[i];
+    }
+
+    dp[0][1] = -prices[0];
+    for(int i=2;i<2*k;i+=2) {
+        dp[0][k] = 0;
+        dp[0][k+1] = -prices[0];
+    }
+    
+    for(int i=1;i<n;i++) {
+        for(int j=2*k-2;j>0;j-=2) {
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j+1] + prices[i]);
+            dp[i][j+1] = max(dp[i-1][j+1], dp[i-1][j-2] - prices[i]);
+        }
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
+        dp[i][1] = max(dp[i-1][1], -prices[i]);
+    }
+
+    cout << dp[n-1][2*k-2] << endl;
+
+    return 0;
+}
+
+void one_index() {
+    int n, k;
+    cin >> n >> k;
     vector<int> dp(2*k, 0);
     vector<int> prices(n, 0);
     for(int i=0;i<n;i++) {
@@ -30,6 +59,4 @@ int main() {
     }
 
     cout << dp[2*k-2] << endl;
-
-    return 0;
 }
