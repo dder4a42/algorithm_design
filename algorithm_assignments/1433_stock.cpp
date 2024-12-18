7cae6c0e1bda46c23518d793f7d295991695c276
deng4a42 <deng4a42@admin>
2024-12-18 10:13:57 +0800

    [TLE] swap takes too much time

--- algorithm_assignments/1433_stock.cpp
+++ algorithm_assignments/1433_stock.cpp
@@ -15,32 +15,34 @@ int main() {
     for(int i=0;i<n;i++) {
         cin >> prices[i];
     }
-    int k = n/2;
-    vector<vector<int>> dp(n, vector<int> (k+1, 0));
 
-    dp[0][0] = 0;
-    dp[0][1] = -prices[0];
-    for(int j=2;j<=k;j++) {
-        dp[0][j] = -INF;
-    }
+    // dp[i][j], the maximal profit we can get in the i-th day, if there are j stocks in hand. 
+    int k = n/2;
+    vector<int> dp(k+5, -INF);
+    vector<int> tmp(k+5, -INF);
+
+    dp[0] = 0;
+    dp[1] = -prices[0];
 
     for(int i=1;i<n;i++) {
-        dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i]);
-        for(int j=1;j<k;j++) {
-            // nothing to do
-            //dp[i][j] = dp[i-1][j];
-            // buy stock
-            //dp[i][j] = dp[i-1][j-1] - prices[i];
-            // sell stock
-            //dp[i][j] = dp[i-1][j+1] + prices[i];
-            dp[i][j] = std::max({dp[i-1][j], dp[i-1][j-1] - prices[i], dp[i-1][j+1] + prices[i]});
+        std::fill(tmp.begin(), tmp.end(), -INF);
+        tmp[0] = max(dp[0], dp[1]+prices[i]);
+        if(i <= n/2) {
+            for(int j=1;j<=i+1;j++) {
+                tmp[j] = std::max(dp[j], dp[j-1] - prices[i]);
+                tmp[j] = std::max(tmp[j], dp[j+1] + prices[i]);
+            }
+        }else {
+            for(int j=1;j<=n-i-1;j++) {
+                tmp[j] = std::max(dp[j], dp[j-1] - prices[i]);
+                tmp[j] = std::max(tmp[j], dp[j+1] + prices[i]);
+            }
         }
-        dp[i][k] = std::max(dp[i-1][k], dp[i-1][k-1] - prices[i]);
+        tmp[k] = std::max(dp[k], dp[k-1] - prices[i]);
+        std::swap(dp, tmp);
     }
 
-    cout << dp[n-1][0] << endl;
+    std::cout << dp[0] << endl;
 
-    
-    
     return 0;
 }
